3362edbd1639b92d1e64ef6dcc74c0c0df87e931
Muhammad Haulul A <haululazkiya@gmail.com>
2022-12-29 23:31:39 +0700

    Update main.py

---
+++ Cari Digit/python/main.py
@@ -0,0 +1,15 @@
+// code by @haululazkiyaa
+
+hasil = "false"
+
+x = int(input("x: "))
+n = int(input("n: "))
+
+while n != 0:
+    sisa = int(n % 10)
+    if sisa == x:
+        hasil = "true"
+        n = 0
+    n = int((n - sisa) / 10) 
+    
+print(hasil)
